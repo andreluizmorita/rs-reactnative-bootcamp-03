99339b88906ba7b506ac4ec7c406280b9c59e4a0
André Luiz Morita <andreluizmorita@gmail.com>
2019-05-20 21:06:14 -0300

    Init

---
+++ src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React , { Component } from 'react';
+import { Container } from './styles';
+
+
+
+class Main extends Component {
+  
+  render() {
+    return <Container />;
+  }
+}
+
+export default Main;
