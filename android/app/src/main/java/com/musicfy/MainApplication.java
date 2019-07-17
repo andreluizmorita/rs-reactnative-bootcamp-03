829d797c4a0c86275de6ee053110032ecb4984ad
André Luiz Morita <andreluizmorita@gmail.com>
2019-07-17 17:24:33 -0300

    CodePush

--- android/app/src/main/java/com/musicfy/MainApplication.java
+++ android/app/src/main/java/com/musicfy/MainApplication.java
@@ -3,6 +3,8 @@ package com.musicfy;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
+import com.microsoft.codepush.react.CodePush;
 import com.guichaguri.trackplayer.TrackPlayer;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -17,6 +19,12 @@ import java.util.List;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+        @Override
+        protected String getJSBundleFile() {
+        return CodePush.getJSBundleFile();
+        }
+    
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -26,6 +34,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeOneSignalPackage(),
+            new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
             new TrackPlayer(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage()
--- src/index.js
+++ src/index.js
@@ -1,19 +1,56 @@
 import '~/config/ReactotronConfig';
 import '~/config/StatusBarConfig';
 
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import CodePush from 'react-native-code-push';
+import OneSignal from 'react-native-onesignal';
 
 import Routes from '~/routes';
 import store from '~/store';
 
 import Player from '~/componets/Player';
 
-const App = () => (
-  <Provider store={store}>
-    <Routes />
-    <Player />
-  </Provider>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-export default App;
+    OneSignal.init('9d033903-d8c5-4537-bed1-76886bba25ae');
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived = (notification) => {
+    console.tron.log('Notification received: ', notification);
+  };
+
+  onOpened = (openResult) => {
+    console.tron.log('Message: ', openResult.notification.payload.body);
+    console.tron.log('Data: ', openResult.notification.payload.additionalData);
+    console.tron.log('isActive: ', openResult.notification.isAppInFocus);
+    console.tron.log('openResult: ', openResult);
+  };
+
+  onIds = () => {};
+
+  render() {
+    return (
+      <Provider store={store}>
+        <Routes />
+        <Player />
+      </Provider>
+    );
+  }
+}
+
+export default CodePush({
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+})(App);
