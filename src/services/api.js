61c1ca51b7b431727a9ce9bdd80c08422a6a498e
André Luiz Morita <amorita.ext@vr.com.br>
2019-07-30 11:34:07 -0300

    CodePush

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'http://www.mocky.io/v2',
+  // baseURL: 'http://localhost:3000',
 });
 
 export default api;
--- src/store/sagas/podcasts.js
+++ src/store/sagas/podcasts.js
@@ -5,8 +5,9 @@ import PodcastsActions from '~/store/ducks/podcasts';
 
 export function* load() {
   try {
-    const response = yield call(api.get, 'podcasts');
-
+    // const response = yield call(api.get, 'podcasts');
+    const response = yield call(api.get, '5d3b26703000002d00a29ff2');
+    console.log(response);
     yield put(PodcastsActions.loadSuccess(response.data));
   } catch (err) {
     yield put(PodcastsActions.loadFailure());
